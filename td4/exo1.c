548a431f7ca79f571eea7054a431853c89aca599
SEPT Mg <67376828+sept-mg@users.noreply.github.com>
2023-10-02 20:11:03 +0200

    exo1.c

--- td4/exo1.c
+++ td4/exo1.c
@@ -1,6 +1,10 @@
 #include <time.h>
 #include <stdio.h>
 
+typedef unsigned long clock_t ;
+// d´efinition de constante
+const clock_t CLOCKS_PER_SEC = /* d´epend de l’ impl´ementation */;
+
 void unGrosCalcul()
 {
 
